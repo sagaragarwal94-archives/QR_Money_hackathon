95da774d9893f1b51edf8c8bb5ad5c67be0ca8d6
Tabs16 <taybawasim@gmail.com>
2016-10-23 03:27:43 +0530

    Necessary changes made

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 
 app = Flask(__name__)
-Bootstrap(app)
+app.config.from_object('config')
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+lm= LoginManager(app)
+
+
 
 from app import views, model
--- app/views.py
+++ app/views.py
@@ -1,7 +1,110 @@
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for, flash, session, \
+            abort
+import os
+import base64
+from io import StringIO
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager, UserMixin, login_user, logout_user, \
+                        current_user
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Required, Length, EqualTo
+import onetimepass
+import pyqrcode
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("base.html")
+	return render_template("index.html")
+
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """User registration route."""
+    if current_user.is_authenticated():
+        # if user is logged in we get out of here
+        return redirect(url_for('index'))
+    form = sign_up()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is not None:
+            flash('Username already exists.')
+            return redirect(url_for('register'))
+        # add new user to the database
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        # redirect to the two-factor auth page, passing username in session
+        session['username'] = user.username
+        return redirect(url_for('two_factor_setup'))
+    return render_template('register.html', form=form)	
+
+@app.route('/twofactor')
+def two_factor_setup():
+    if 'username' not in session:
+        return redirect(url_for('index'))
+    user = User.query.filter_by(username=session['username']).first()
+    if user is None:
+        return redirect(url_for('index'))
+    # since this page contains the sensitive qrcode, make sure the browser
+    # does not cache it
+    return render_template('two-factor-setup.html'), 200, {
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'}
+
+
+@app.route('/qrcode')
+def qrcode():
+    if 'username' not in session:
+        abort(404)
+    user = User.query.filter_by(username=session['username']).first()
+    if user is None:
+        abort(404)
+
+    # for added security, remove username from session
+    del session['username']
+
+    # render qrcode for FreeTOTP
+    url = pyqrcode.create(user.get_totp_uri())
+    stream = StringIO()
+    url.svg(stream, scale=3)
+    return stream.getvalue().encode('utf-8'), 200, {
+        'Content-Type': 'image/svg+xml',
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'}
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """User login route."""
+    if current_user.is_authenticated():
+        # if user is logged in we get out of here
+        return redirect(url_for('index'))
+    form = login()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.verify_password(form.password.data) or \
+                not user.verify_totp(form.token.data):
+            flash('Invalid username, password or token.')
+            return redirect(url_for('login'))
+
+        # log user in
+        login_user(user)
+        flash('You are now logged in!')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """User logout route."""
+    logout_user()
+    return redirect(url_for('index'))
