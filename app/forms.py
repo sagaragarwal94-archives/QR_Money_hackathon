00c00c3f9f408ae7676d4365b8e53ea85c42e153
himanshi <himanshikhandelwal8@gmail.com>
2016-10-23 03:00:33 +0530

    database

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,20 @@
 from flask_wtf import Form
-from wtforms import StringField, DateTimeField, PasswordField, IntegerField
+from wtforms import StringField, DateTimeField, PasswordField, IntegerField,SubmitField
 from wtforms.validators import DataRequired
 
 
 class sign_up(Form):
     email=StringField('Email',validators=[DataRequired()])
     phone=IntegerField('Number', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Password again',
+                                   validators=[Required(), EqualTo('password')])
+    submit = SubmitField('Register')
    # otp=IntegerField(('xxxx'), validators=[DataRequired()])
 
 
 class login(Form):
-	phone=IntegerField('Number', validators=[DataRequired()])
-	otp=IntegerField('xxxx', validators=[DataRequired()])
-	
+	username = StringField('Username', validators=[Required(), Length(1, 64)])
+    password = PasswordField('Password', validators=[Required()])
+    token = StringField('Token', validators=[Required(), Length(6, 6)])
+    submit = SubmitField('Login')
--- app/model.py
+++ app/model.py
@@ -3,3 +3,35 @@ class user(db.Model):
     email=db.Column(db.String(1000),unique=True)
     phone=db.Column(db.Int(50),unique=True)
     credits=db.Column(db.Int(50))
+    password=db.Column(db.String(25),unique=True)
+    otp_secret = db.Column(db.String(16))
+
+     def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.otp_secret is None:
+            # generate a random secret
+            self.otp_secret = base64.b32encode(os.urandom(10)).decode('utf-8')
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_totp_uri(self):
+        return 'otpauth://totp/2FA-Demo:{0}?secret={1}&issuer=2FA-Demo' \
+            .format(self.username, self.otp_secret)
+
+    def verify_totp(self, token):
+        return onetimepass.valid_totp(token, self.otp_secret)
+
+
+@lm.user_loader
+def load_user(user_id):
+    """User loader callback for Flask-Login."""
+    return User.query.get(int(user_id))
